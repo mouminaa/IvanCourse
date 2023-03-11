f974c47dd25925d4b3e7ad327ef5b1e301a688c6
mouminaa <114490920+mouminaa@users.noreply.github.com>
2023-03-11 16:10:51 +0300

    translate

--- all.py
+++ all.py
@@ -2,25 +2,32 @@ import tika
 from tika import parser
 
 # Initialisation de Tika
+# Tika initialization
 tika.initVM()
 
+
 # Liste des chemins vers les fichiers à traiter
-chemins_fichiers = ['momo.pdf', 'DBSCAN.docx']
+# List of paths to files to process
+files_path = ['momo.pdf', 'DBSCAN.docx']
 
 # Boucle sur les fichiers
-for chemin_fichier in chemins_fichiers:
+# Loop over files
+for file_path in files_path:
     # Analyse du fichier avec Tika
-    contenu_fichier = parser.from_file(chemin_fichier)
+    # Analysis of the file with Tika
+    file_content = parser.from_file(file_path)
     
     # Récupération du contenu textuel
-    contenu_textuel = contenu_fichier['content']
+    # Retrieval of textual content
+    text_content = file_content['content']
     
     # Récupération des métadonnées
-    metadonnees = contenu_fichier['metadata']
+    # Metadata retrieval
+    metadata = file_content['metadata']
     
     # Affichage du contenu textuel et des métadonnées
-    print(f"Contenu textuel de {chemin_fichier}:")
-    print(contenu_textuel)
+    print(f"textual content of{file_path}:")
+    print(text_content)
     print("Métadonnées:")
-    for cle, valeur in metadonnees.items():
+    for cle, valeur in metadata.items():
         print(f"{cle}: {valeur}")
