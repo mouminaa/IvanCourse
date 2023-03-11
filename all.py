e5eb43e2c5d0b71223ffe31b62733bc189042f42
Takudzwamz <zhout1709@hotmail.com>
2023-03-11 15:10:31 +0300

    House cleaning

--- all.py
+++ all.py
@@ -1,33 +1,50 @@
+import pytest
 import tika
 from tika import parser
-
-# Initialisation de Tika
-# Tika initialization
-tika.initVM()
+import os
 
 
-# Liste des chemins vers les fichiers à traiter
-# List of paths to files to process
-files_path = ['momo.pdf', 'DBSCAN.docx', 'sample2.djvu']
+def test_parser():
+    # Initialise Tika
+    tika.initVM()
 
-# Boucle sur les fichiers
-# Loop over files
-for file_path in files_path:
-    # Analyse du fichier avec Tika
-    # Analysis of the file with Tika
-    file_content = parser.from_file(file_path)
-    
-    # Récupération du contenu textuel
-    # Retrieval of textual content
-    text_content = file_content['content']
-    
-    # Récupération des métadonnées
-    # Metadata retrieval
-    metadata = file_content['metadata']
-    
-    # Affichage du contenu textuel et des métadonnées
-    print(f"textual content of{file_path}:")
-    print(text_content)
-    print("Métadonnées:")
-    for cle, valeur in metadata.items():
-        print(f"{cle}: {valeur}")
+    # List of paths to files to process
+    files_path = ['input/momo.pdf', 'input/DBSCAN.docx', 'input/test.djvu']
+
+    # Output directory path
+    output_dir_path = "output"
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir_path):
+        os.makedirs(output_dir_path)
+
+    # Loop over files
+    for file_path in files_path:
+        # Analysis of the file with Tika
+        file_content = parser.from_file(file_path)
+
+        # Retrieval of textual content
+        text_content = file_content['content']
+
+        # Metadata retrieval
+        metadata = file_content['metadata']
+
+        # Get the file extension
+        file_extension = os.path.splitext(file_path)[1]
+
+        # Create the output file path
+        output_file_path = os.path.join(
+            output_dir_path, f"{file_extension}.txt")
+
+        # Open the output file for writing with UTF-8 encoding
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
+            # Write the textual content and metadata to the output file
+            output_file.write(f"textual content of {file_path}:\n")
+            if text_content is not None:
+                output_file.write(text_content)
+            output_file.write("\nMeta Data:\n")
+            for cle, valeur in metadata.items():
+                output_file.write(f"{cle}: {valeur}\n")
+
+        # Check if the output file was created successfully
+        assert os.path.exists(output_file_path)
--- extract_Adresse_HTTP.py
+++ extract_Adresse_HTTP.py
@@ -1,25 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import os
 
 
 def get_links(url):
+    # Send a GET request to the URL and retrieve the response
     response = requests.get(url)
+    # Create a BeautifulSoup object from the response content
     soup = BeautifulSoup(response.content, 'html.parser')
+    # Find all the <a> tags in the HTML
     links = soup.find_all('a')
+    # Extract the 'href' attribute from each of the tags
     return [link.get('href') for link in links]
 
 
 def test_get_links():
+    # Define the URL to test
     url = "https://sputniktech.co"
+    # Define the expected output
     expected_output = ['/projects', '/blogs', '/team', '/projects', '/', '/blogs', '/projects', '/contact_us', '/', 'https://www.facebook.com/sputniktechspb/', 'https://www.instagram.com/sputnikmupa/',
                        'https://www.linkedin.com/company/sputnik-tech/', 'https://wa.me/message/4QJ2NGZ7FJQZF1', 'https://wa.me/message/4B662PIKJJV4K1', 'tel:+27682233859', '/cdn-cgi/l/email-protection', 'https://sputniktech.co/', None]
+    # Call the get_links function
     output = get_links(url)
-    with open('links.csv', 'w', newline='') as csvfile:
+    # Create the 'output' folder if it doesn't exist already
+    os.makedirs('output', exist_ok=True)
+    # Write the output to a CSV file in the 'output' folder
+    with open('output/links.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['Link'])
+        # Write the header row
+        writer.writerow(['Links'])
+        # Write each link as a row in the CSV file
         for link in output:
             writer.writerow([link])
+    # Print the output and expected output        
     print("Actual output: ", output)
     print("Expected output: ", expected_output)
+    # Check if the output matches the expected output
     assert output == expected_output
