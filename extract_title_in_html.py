5eae2164832917cd60f626726910321187cb8adb
Takudzwamz <zhout1709@hotmail.com>
2023-03-11 12:40:30 +0300

    Added unit tests for Html parcer

--- extract_title_in_html.py
+++ extract_title_in_html.py
@@ -6,17 +6,22 @@ def extract_article_titles(html):
     titles = soup.find_all(['h1', 'h2', 'h3'])
     return [title.get_text() for title in titles]
 
-def main(page):
-    with open(page) as f:
-        html = f.read()
-    titles = extract_article_titles(html)
-    with open('article_titles.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Title:'])
-        for title in titles:
-            writer.writerow([title])
 
-if __name__ == '__main__':
-    main('contact.html') 
-    main('Page.html') 
+
+# `pytest extract_title_in_html.py::test_extract_article_titles` to run the test
+def test_extract_article_titles():
+    with open('test.html', 'r') as file:
+        html = file.read()
+    expected_output = ['Test Heading 1', 'Test Heading 2',
+                       'Test Heading 3', 'Another Test Heading 2']
+    output = extract_article_titles(html)
+    with open('titles.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Title'])
+        for title in output:
+            writer.writerow([title])
+    assert output == expected_output
+    
+    
+
     
